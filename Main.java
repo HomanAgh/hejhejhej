19353db4f3fe92836c90a4297e3271f3c6687f40
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2023-09-20 14:58:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        adderaMonopolGator();
+        adderaMonopolGator();
+        adderaMonopolGator();
+
+
+    }
+    private static void adderaMonopolGator() {
+        System.out.println("Ringvägen, Norrmalmstorg, Södra Station");
+    }
+
+
+}
+
+        /*int a = 1;
+        switch(a){
+            case 1:
+                System.out.println("Rätt");
+                break;
+            default:
+                System.out.println("Fel");
+        }
+
+        int Namn = 1;
+
+        switch (Namn) {
+            case 1 -> System.out.println("Pontus");
+
+            default -> System.out.println("Invalid");
+
+        }
+
+        public class miniRäknare {
+            public static int miniRäknare(int x, int y) {
+                int summa = x * y;
+                return summa;
+            }
+            public static void main(String[] args) {
+                int summa = miniRäknare(6, 6);
+                System.out.println("summan blir: " + summa );
+            }
+        }*/
+
+
